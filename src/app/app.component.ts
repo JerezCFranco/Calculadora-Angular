491500ae54d4e9098832dea28e36a679d2f61cae
JerezCFranco <ermac715@gmail.com>
2024-02-28 21:17:13 -0300

    Calculadora Hecha en Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink,CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Calculadora';
+export class AppComponent implements OnInit{
+  pantallaValor: string = '';
+
+  constructor(private titleCalculadora: Title) {}
+
+  ngOnInit(): void {
+    this.titleCalculadora.setTitle('Calculadora');
+  }
+
+  agregar(valor: string): void {
+    this.pantallaValor += valor;
+  }
+
+  borrar(): void {
+    this.pantallaValor = '';
+  }
+
+  borrarUltimo(): void {
+    if (this.pantallaValor.length > 0) {
+      this.pantallaValor = this.pantallaValor.slice(0, -1);
+    }
+  }
+
+  calcular(): void {
+    try {
+      const resultado = new Function('return ' + this.pantallaValor)();
+      this.pantallaValor = resultado.toString();
+    } catch (error) {
+      console.error('Error en el cálculo:', error);
+    }
+  }
+
 }
